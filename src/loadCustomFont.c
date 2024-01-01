d329dfd0d7fc7749a35e1366291f8243402b881d
realzvqle <eyefort@gmail.com>
2024-01-01 16:21:59 -0600

    st

--- src/loadCustomFont.c
+++ src/loadCustomFont.c
@@ -4,6 +4,5 @@
 
 
 void figeDrawText(gameState* gameState, char* text, int width, int height, float size, Color color){
-    //gameState->font.size = size;
     DrawTextEx(gameState->font.font, text, (Vector2){width, height}, size, 1, color);
 }
--- src/screens.c
+++ src/screens.c
@@ -5,7 +5,6 @@
 void drawMenuScreen(gameState* gameState){
     figeDrawText(gameState, "FigeChaser", (gameState->window.size.x - MeasureText("FigeChaser", 70)) / 2, gameState->window.size.y / 2 - 40, 70, GREEN);
     figeDrawText(gameState, "Press Space To Start", (gameState->window.size.x - MeasureText("Press Space To Start", 40)) / 2, gameState->window.size.y / 2 + 80, 40, BLUE);
-
     if(IsKeyDown(KEY_SPACE)) gameState->scene.currentScene = 1;
 }
 
